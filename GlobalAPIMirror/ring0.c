3d7c5644fde75a78a5f9599b272bb35813fcb310
PYozo <50827329+PYozo@users.noreply.github.com>
2021-02-10 17:30:22 +0800

    Update ring0.c

--- GlobalAPIMirror/ring0.c
+++ GlobalAPIMirror/ring0.c
@@ -33,6 +33,69 @@ typedef struct
 }LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
 
 _PspTerminateProcess PspTerminateProcess;
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING reg_path);
+UINT32 *GetPDE(UINT32 addr);
+UINT32 *GetPTE(UINT32 addr);
+VOID DriverUnload(PDRIVER_OBJECT pDriver);
+NTSTATUS IrpCreateProc(PDEVICE_OBJECT pDevObj, PIRP pIrp);
+NTSTATUS IrpCloseProc(PDEVICE_OBJECT pDevObj ,PIRP pIrp);
+NTSTATUS IrpDeviceControlProc(PDEVICE_OBJECT pDevObj, PIRP pIrp);
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING reg_path)
+{
+    NTSTATUS status;
+    PDEVICE_OBJECT pDeviceObj;
+    UNICODE_STRING Devicename;
+    UNICODE_STRING SymbolicLinkName;
+    PVOID KernelBase=NULL;
+    UINT32 uKernelImageSize;
+
+    //初始化设备名
+    RtlInitUnicodeString(&Devicename, DEVICE_NAME);
+    //创建设备对象
+    status=IoCreateDevice(pDriver, 0, &Devicename, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE,&pDeviceObj);
+
+    pDeviceObj->Flags |= DO_BUFFERED_IO;
+
+    if (status != STATUS_SUCCESS)
+    {
+        IoDeleteDevice(pDeviceObj);
+        DbgPrint("创建设备失败.\n");
+        return status;
+    }
+
+    //创建符号链接名称
+    RtlInitUnicodeString(&SymbolicLinkName, SYMBOLICLINK_NAME);
+    //创建符号链接
+    IoCreateSymbolicLink(&SymbolicLinkName, &Devicename);
+
+    //创建分发函数
+    pDriver->DriverUnload = DriverUnload;
+    pDriver->MajorFunction[IRP_MJ_CREATE] = IrpCreateProc;
+    pDriver->MajorFunction[IRP_MJ_CLOSE] = IrpCloseProc;
+    pDriver->MajorFunction[IRP_MJ_DEVICE_CONTROL] = IrpDeviceControlProc;
+    
+
+    return STATUS_SUCCESS;
+}
+
+UINT32 *GetPDE(UINT32 addr)
+{
+    //return (DWORD *)(0xc0600000 + ((addr >> 18) & 0x3ff8));
+    UINT32 PDPTI = addr >> 30;
+    UINT32 PDI = (addr >> 21) & 0x000001FF;
+    UINT32 PTI = (addr >> 12) & 0x000001FF;
+    return (UINT32 *)(0xC0600000 + PDPTI * 0x1000 + PDI * 8);
+}
+
+UINT32 *GetPTE(UINT32 addr)
+{
+    //return (DWORD *)(0xc0000000 + ((addr >> 9) & 0x7ffff8));
+    UINT32 PDPTI = addr >> 30;
+    UINT32 PDI = (addr >> 21) & 0x000001FF;
+    UINT32 PTI = (addr >> 12) & 0x000001FF;
+    return (UINT32 *)(0xC0000000 + PDPTI * 0x200000 + PDI * 0x1000 + PTI * 8);
+}
 
 VOID DriverUnload(PDRIVER_OBJECT pDriver)
 {
@@ -67,6 +130,7 @@ NTSTATUS IrpDeviceControlProc(PDEVICE_OBJECT pDevObj, PIRP pIrp)
 
     PIO_STACK_LOCATION pIrpStack;
     PVOID pIoBuffer;
+	UINT32* phyAddr;
     ULONG uIoControlCode;
     ULONG uOutLength;
     ULONG uInLength;
@@ -100,75 +164,19 @@ NTSTATUS IrpDeviceControlProc(PDEVICE_OBJECT pDevObj, PIRP pIrp)
         }
         case OPER2:
         {
-            __asm int 3;
             DbgPrint("IrpDeviceControlProc -> OPER2 输入字节数: %d\n", uInLength);
             DbgPrint("IrpDeviceControlProc -> OPER2 输出字节数: %d\n", uOutLength);
             memcpy(&uRead, pIoBuffer, 4);
-            DbgPrint("%x", uRead);
-            PsLookupProcessByProcessId((HANDLE)uRead, &pE);
-            status=PspTerminateProcess(pE,0);
-            DbgPrint("%x", status);
-            pIrp->IoStatus.Information = 2; // 返回两字节
+			DbgPrint("%d\n",uRead);
+			*GetPDE(uRead)=*phyAddr|0x867;
+			*GetPTE(uRead)=*phyAddr|0x867;
+			uRead=200;
+			memcpy(pIoBuffer,&uRead,4);
+            pIrp->IoStatus.Information = 4; // 返回两字节
             status = STATUS_SUCCESS;
             break;
         }
     }
-
-    return STATUS_SUCCESS;
-}
-
-UINT32 *GetPDE(UINT32 addr)
-{
-    //return (DWORD *)(0xc0600000 + ((addr >> 18) & 0x3ff8));
-    UINT32 PDPTI = addr >> 30;
-    UINT32 PDI = (addr >> 21) & 0x000001FF;
-    UINT32 PTI = (addr >> 12) & 0x000001FF;
-    return (UINT32 *)(0xC0600000 + PDPTI * 0x1000 + PDI * 8);
-}
-
-UINT32 *GetPTE(UINT32 addr)
-{
-    //return (DWORD *)(0xc0000000 + ((addr >> 9) & 0x7ffff8));
-    UINT32 PDPTI = addr >> 30;
-    UINT32 PDI = (addr >> 21) & 0x000001FF;
-    UINT32 PTI = (addr >> 12) & 0x000001FF;
-    return (UINT32 *)(0xC0000000 + PDPTI * 0x200000 + PDI * 0x1000 + PTI * 8);
-}
-extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING reg_path)
-{
-    NTSTATUS status;
-    PDEVICE_OBJECT pDeviceObj;
-    UNICODE_STRING Devicename;
-    UNICODE_STRING SymbolicLinkName;
-    PVOID KernelBase=NULL;
-    UINT32 uKernelImageSize;
-    
-    //初始化设备名
-    RtlInitUnicodeString(&Devicename, DEVICE_NAME);
-
-    //创建设备对象
-    status=IoCreateDevice(pDriver, 0, &Devicename, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE,&pDeviceObj);
-
-    pDeviceObj->Flags |= DO_BUFFERED_IO;
-
-    if (status != STATUS_SUCCESS)
-    {
-        IoDeleteDevice(pDeviceObj);
-        DbgPrint("创建设备失败.\n");
-        return status;
-    }
-
-    //创建符号链接名称
-    RtlInitUnicodeString(&SymbolicLinkName, SYMBOLICLINK_NAME);
-    //创建符号链接
-    IoCreateSymbolicLink(&SymbolicLinkName, &Devicename);
-
-    //创建分发函数
-    pDriver->DriverUnload = DriverUnload;
-    pDriver->MajorFunction[IRP_MJ_CREATE] = IrpCreateProc;
-    pDriver->MajorFunction[IRP_MJ_CLOSE] = IrpCloseProc;
-    pDriver->MajorFunction[IRP_MJ_DEVICE_CONTROL] = IrpDeviceControlProc;
-    
 
     return STATUS_SUCCESS;
 }
